76de5bbe464a8035fe66d900be8c2d5c99720913
I54m <i54m@i54m.com>
2026-01-25 19:57:34 +1300

    Remove manual check for item material

--- src/main/java/net/vorplex/core/towny/plotvouchers/PlayerInteractListener.java
+++ src/main/java/net/vorplex/core/towny/plotvouchers/PlayerInteractListener.java
@@ -33,42 +33,40 @@ public class PlayerInteractListener implements Listener {
         if (event.getHand() != null && event.getHand().equals(EquipmentSlot.HAND)
                 && (event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK))) {
             Player player = event.getPlayer();
-            if (player.getInventory().getItemInMainHand().getType().equals(Material.PAPER)) {
-                ItemStack item = player.getInventory().getItemInMainHand();
-                if (isVoucherItem(item)) {
-                    event.setCancelled(true);
-                    Resident resident;
-                    Town town;
-                    try {
-                        resident = TownyAPI.getInstance().getResident(player.getUniqueId());
-                        if (resident == null) throw new Exception("Resident is null!");
-                        town = resident.getTown();
-                    } catch (Exception ignored) {
-                        player.sendMessage(Component.text("Sorry you are not in a town so you can't redeem plot vouchers, do /t new to create one").color(NamedTextColor.RED));
-                        return;
-                    }
-                    if (confirm.containsKey(player) && System.currentTimeMillis() <= confirm.get(player)) {
-                        confirm.remove(player);
-                        int amount = item.getAmount();
-                        player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
-                        player.updateInventory();
-                        town.addBonusBlocks(amount);
-                        TownyAPI.getInstance().getDataSource().saveTown(town);
-                        player.sendMessage(Component.text("You redeemed " + amount + " bonus town blocks for your town: " + town.getName()).color(NamedTextColor.GREEN));
-                        player.playSound(Sound.sound(Key.key("minecraft", "entity.player.levelup"), Sound.Source.MASTER, 0.5f, 1.0f));
-                        Resident mayor = town.getMayor();
-                        Player mayorPlayer = Bukkit.getPlayer(mayor.getUUID());
-                        if (mayorPlayer != null && !mayorPlayer.equals(player))
-                            mayorPlayer.sendMessage(Component.text(player.getName() + " just redeemed " + item.getAmount() + " plot vouchers for your town!").color(NamedTextColor.GREEN));
-                    } else if (!confirm.containsKey(player)) {
-                        player.sendMessage(Component.text("Please confirm you would like to redeem " + item.getAmount() + " plot vouchers for the town " + town.getName() + " this cannot be undone!").color(NamedTextColor.RED));
-                        confirm.put(player, (System.currentTimeMillis() + 30000));
-                    } else {
-                        confirm.remove(player);
-                        player.sendMessage(Component.text("Your 30s to confirm expired!").color(NamedTextColor.RED));
-                        player.sendMessage(Component.text("Please confirm you would like to redeem " + item.getAmount() + " plot vouchers for the town " + town.getName() + " this cannot be undone!").color(NamedTextColor.RED));
-                        confirm.put(player, (System.currentTimeMillis() + 30000));
-                    }
+            ItemStack item = player.getInventory().getItemInMainHand();
+            if (isVoucherItem(item)) {
+                event.setCancelled(true);
+                Resident resident;
+                Town town;
+                try {
+                    resident = TownyAPI.getInstance().getResident(player.getUniqueId());
+                    if (resident == null) throw new Exception("Resident is null!");
+                    town = resident.getTown();
+                } catch (Exception ignored) {
+                    player.sendMessage(Component.text("Sorry you are not in a town so you can't redeem plot vouchers, do /t new to create one").color(NamedTextColor.RED));
+                    return;
+                }
+                if (confirm.containsKey(player) && System.currentTimeMillis() <= confirm.get(player)) {
+                    confirm.remove(player);
+                    int amount = item.getAmount();
+                    player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
+                    player.updateInventory();
+                    town.addBonusBlocks(amount);
+                    TownyAPI.getInstance().getDataSource().saveTown(town);
+                    player.sendMessage(Component.text("You redeemed " + amount + " bonus town blocks for your town: " + town.getName()).color(NamedTextColor.GREEN));
+                    player.playSound(Sound.sound(Key.key("minecraft", "entity.player.levelup"), Sound.Source.MASTER, 0.5f, 1.0f));
+                    Resident mayor = town.getMayor();
+                    Player mayorPlayer = Bukkit.getPlayer(mayor.getUUID());
+                    if (mayorPlayer != null && !mayorPlayer.equals(player))
+                        mayorPlayer.sendMessage(Component.text(player.getName() + " just redeemed " + item.getAmount() + " plot vouchers for your town!").color(NamedTextColor.GREEN));
+                } else if (!confirm.containsKey(player)) {
+                    player.sendMessage(Component.text("Please confirm you would like to redeem " + item.getAmount() + " plot vouchers for the town " + town.getName() + " this cannot be undone!").color(NamedTextColor.RED));
+                    confirm.put(player, (System.currentTimeMillis() + 30000));
+                } else {
+                    confirm.remove(player);
+                    player.sendMessage(Component.text("Your 30s to confirm expired!").color(NamedTextColor.RED));
+                    player.sendMessage(Component.text("Please confirm you would like to redeem " + item.getAmount() + " plot vouchers for the town " + town.getName() + " this cannot be undone!").color(NamedTextColor.RED));
+                    confirm.put(player, (System.currentTimeMillis() + 30000));
                 }
             }
         }
